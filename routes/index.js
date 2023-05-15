7f41b68061b14dc9b6368671334e6c65765e4775
hriveraperu <gioxshaoran@hotmail.com>
2023-05-15 01:28:04 -0500

    =)

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,7 @@ routes.use(
   '/',
   (docData = (req, res) => {
     let docData = {
-      documentationURL: 'https://nathanbirch.github.io/nathan-byui-api-docs',
+      documentationURL: 'https://wdd330-alerts.onrender.com/api-docs',
     };
     res.send(docData);
   })
