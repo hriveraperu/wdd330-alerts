26ca949f8449430f5a7b71f1f4088e3d3d2fb205
hriveraperu <gioxshaoran@hotmail.com>
2023-05-18 21:13:31 -0500

    updating coding

--- swagger.js
+++ swagger.js
@@ -5,8 +5,8 @@ const doc = {
     title: 'My API',
     description: 'Description',
   },
-  host: 'localhost:8080',
-  schemes: ['http'],
+  host: 'https://wdd330-alerts.onrender.com/',
+  schemes: ['https'],
 };
 
 const outputFile = './swagger-output.json';
